3672264622521fdac647c44ed0a8cc4ae6012031
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2024-12-01 21:48:35 +0000

    (Day 01) added verbose flag for debugging example

--- day_01/solution_A.py
+++ day_01/solution_A.py
@@ -1,26 +1,33 @@
 from typing import List, Tuple
 
 
-def total_difference(vertical_lists: List[Tuple[int, int]]) -> int:
+def total_difference(vertical_lists: List[Tuple[int, int]], verbose: bool = False) -> int:
     left, right = list(zip(*vertical_lists))
 
     differences = [
         abs(a - b)
         for a, b in zip(sorted(left), sorted(right))]
 
+    if verbose:
+        print(" + ".join(map(str, differences)))
+
     return sum(differences)
 
 
 if __name__ == "__main__":
-    # test_input = [
-    #     (3, 4),
-    #     (4, 3),
-    #     (2, 5),
-    #     (1, 3),
-    #     (3, 9),
-    #     (3, 3)]
+    test_input = [
+        (3, 4),
+        (4, 3),
+        (2, 5),
+        (1, 3),
+        (3, 9),
+        (3, 3)]
 
-    # print(total_difference(test_input))  # 11
+    print("\nexample:")
+    print(total_difference(test_input, verbose=True))
+    # expected output:
+    # -- 2 + 1 + 0 + 1 + 2 + 5
+    # -- 11
 
     two_lists = list()
     with open("historic_locations.txt") as input_file:
@@ -28,4 +35,5 @@ if __name__ == "__main__":
             tuple(map(int, line.split("  ")))
             for line in input_file]
 
+    print("\nmain:")
     print(total_difference(two_lists))
--- day_01/solution_B.py
+++ day_01/solution_B.py
@@ -1,28 +1,37 @@
 from typing import List, Tuple
 
 
-def similarity_score(vertical_lists: List[Tuple[int, int]]) -> int:
+def similarity_score(vertical_lists: List[Tuple[int, int]], verbose: bool = False) -> int:
     left, right = list(zip(*vertical_lists))
 
     counts = {
         a: right.count(a)
         for a in set(left)}
 
-    return sum(
+    scores = [
         a * counts[a]
-        for a in left)
+        for a in left]
+
+    if verbose:
+        print(" + ".join(map(str, scores)))
+
+    return sum(scores)
 
 
 if __name__ == "__main__":
-    # test_input = [
-    #     (3, 4),
-    #     (4, 3),
-    #     (2, 5),
-    #     (1, 3),
-    #     (3, 9),
-    #     (3, 3)]
+    test_input = [
+        (3, 4),
+        (4, 3),
+        (2, 5),
+        (1, 3),
+        (3, 9),
+        (3, 3)]
 
-    # print(similarity_score(test_input))  # 11
+    print("\nexample:")
+    print(similarity_score(test_input, verbose=True))
+    # expected ouput:
+    # -- 9 + 4 + 0 + 0 + 9 + 9
+    # -- 31
 
     two_lists = list()
     with open("historic_locations.txt") as input_file:
@@ -30,4 +39,5 @@ if __name__ == "__main__":
             tuple(map(int, line.split("  ")))
             for line in input_file]
 
+    print("\nmain:")
     print(similarity_score(two_lists))
