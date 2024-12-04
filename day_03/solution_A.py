9a4c5b19467a53446e64da3b78fb4f3dec151773
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2024-12-04 18:02:19 +1000

    (Day 03) cleanup

--- day_03/solution_A.py
+++ day_03/solution_A.py
@@ -5,7 +5,7 @@ def parse(memory: str, verbose: bool = False) -> int:
     pattern = re.compile(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)")
     muls = pattern.findall(memory)
     if verbose:
-        print(f"mul({a}, {b})" for a, b in muls)
+        print(f"mul({a},{b})" for a, b in muls)
     return sum([
         int(a) * int(b)
         for a, b in muls])
@@ -19,6 +19,11 @@ if __name__ == "__main__":
     print("\nexample:")
     print(parse(test_input, verbose=True))
     # expected output:
+    # -- mul(2,4)
+    # -- mul(5,5)
+    # -- mul(11,8)
+    # -- mul(8,5)
+    # -- 161
 
     with open("memory.txt") as input_file:
         memory = input_file.read()
